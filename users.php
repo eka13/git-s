d89fcd53fbef7f4e322fd8026265000eefa13f74
eka1308 <eka1308@yandex.ru>
2015-10-11 00:15:58 +0400

    страница - список зарешистрированных пользователей

---
+++ users.php
@@ -0,0 +1,17 @@
+﻿<?php
+  require_once('shab.php');
+  require_once('class_users.php');
+  ?>
+ <div class="container">  
+ <?php 
+  if (isset($_SESSION['valid_user'])) {
+  $user_email = $_SESSION['valid_user'];
+  echo '<h1>Люди</h1>';
+  $my = new users();
+  $my->getUsers().'<br/>';
+} else {
+    echo '<p>Вы не вошли в систему.</p>';
+}
+  echo '<a href="index.php">На главную страницу</a>';
+?>
+ </div>
