d9690dec63f3c4a588261bdf2d6dd9a8e4e34ed5
eka1308 <eka1308@yandex.ru>
2015-10-11 00:08:55 +0400

    страница список событий

---
+++ events.php
@@ -0,0 +1,17 @@
+﻿﻿<?php
+  require_once('shab.php');
+  require_once('class_events.php');
+   ?>
+ <div class="container">  
+ <?php 
+  if (isset($_SESSION['valid_user'])) {
+  $user_email = $_SESSION['valid_user'];
+  echo '<h1>События</h1>';
+  $ev = new events();
+  $ev->getEvents().'<br/>';
+} else {
+    echo '<p>Вы не вошли в систему.</p>';
+}
+  echo '<a href="index.php">На главную страницу</a>';
+?>
+ </div>
