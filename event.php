2ffff02d6560eae66d4312e6e1c0945f1b7a1e4a
eka1308 <eka1308@yandex.ru>
2015-10-11 00:08:23 +0400

    Страница событие

---
+++ event.php
@@ -0,0 +1,16 @@
+﻿<?php
+  session_start();
+  require_once('class_event.php');
+  if (isset($_SESSION['valid_user'])) {
+  $user_email = $_SESSION['valid_user'];
+  $event_id = $_GET['event_id'];
+  echo '<h1>Событие</h1>';
+  $evnt = new event();
+  $evnt->getEvents($event_id).'<br/>';
+  echo '<a href="index.php">На главную страницу</a>';
+} else {
+    echo '<p>Вы не вошли в систему.</p>';
+	echo '<a href="index.php">На главную страницу</a>';
+}
+  
+?>
